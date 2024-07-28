60f98205f1cc72e12666fc2a24cad81f4f3a1564
giraySipit <sipitgiray@gmail.com>
2024-07-29 02:20:12 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Giray Şipit on 15.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
